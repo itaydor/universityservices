1729cdeeff1625360415b166121b6ecd2f6a8091
itaydor <108480612+itaydor@users.noreply.github.com>
2022-08-10 22:58:27 +0300

    add curse controller tests.

--- curse/src/test/java/com/ius/curse/CurseControllerTest.java
+++ curse/src/test/java/com/ius/curse/CurseControllerTest.java
@@ -1,35 +1,41 @@
 package com.ius.curse;
 
+import com.ius.clients.curse.CurseGetByNameResponse;
+import com.ius.curse.request.CurseAddRequest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Collections;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(CurseController.class)
 class CurseControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private CurseService curseService;
 
-    @Mock
+    @MockBean
     private CurseRepository curseRepository;
 
     @Test
-    @Disabled
     void shouldGetAllCurses() throws Exception {
         //given
         String curseName = "Curse_Name";
@@ -39,17 +45,88 @@ class CurseControllerTest {
         when(curseService.getCurses())
                 .thenReturn(Collections.singletonList(curse));
 
-        mockMvc.perform(get(""))
-                .andExpect(status().isOk());
+        //when
+        //then
+        mockMvc.perform(get("/api/v1/curse"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(curseName))
+                );
+
+        verify(curseService).getCurses();
     }
 
     @Test
-    @Disabled
-    void shouldAddCurse() {
+    void shouldAddCurse() throws Exception {
+        //given
+        String curseName = "Curse_Name";
+        CurseAddRequest curseAddRequest = new CurseAddRequest(curseName);
+
+        //when
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/v1/curse")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(String.format("{\"curseName\": \"%s\"}", curseName))
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        verify(curseService).addCurse(curseAddRequest);
     }
 
     @Test
-    @Disabled
-    void shouldGetCurseByName() {
+    void shouldGetCurseByName() throws Exception {
+        //given
+        String curseName = "Curse_Name";
+        int curseID = 1;
+        boolean isExists = true;
+
+        Curse curse = Curse.builder()
+                .curseID(curseID)
+                .name(curseName)
+                .build();
+        Optional<Curse> optionalCurse = Optional.of(curse);
+
+        when(curseService.getCurseByName(curseName)).thenReturn(optionalCurse);
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders
+                                .get("/api/v1/curse/" + curseName))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.isExists", is(isExists)))
+                .andExpect(jsonPath("$.curseID", is(curseID)))
+                .andDo(print());
+
+        //then
+        verify(curseService).getCurseByName(curseName);
+    }
+
+    @Test
+    void shouldReturnEmptyGetCurseByName() throws Exception {
+        //given
+        String curseName = "Curse_Name";
+        int curseID = 0;
+        boolean isExists = false;
+
+        Optional<Curse> optionalCurse = Optional.empty();
+
+        when(curseService.getCurseByName(curseName)).thenReturn(optionalCurse);
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/curse/" + curseName))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.isExists", is(isExists)))
+                .andExpect(jsonPath("$.curseID", is(curseID)))
+                .andDo(print());
+
+        //then
+        verify(curseService).getCurseByName(curseName);
     }
 }
